51d88d0eb6ed00eb975680e750a8e4e570c193d6
thaumiel1 <williamkane562@gmail.com>
2026-02-10 15:05:07 +0000

    wav ingest working, stereo to mono conversion working, working on splitting into frequency bands for analysis

--- devlog.md
+++ devlog.md
@@ -1,4 +1,5 @@
 # 2026-02-01 19:00:00.000
 Started work on this project. Initial idea is to capture data and store it in a deque containing chunks of samples. Proving to be unergonomic, switching to interpreting a .wav file with a sliding window to imitate live response to signals.
 
-#
+# 2026-02-10T15:02:33+0000
+Wav file ingest complete, mono conversion complete, working on band pass filters to split signal into bands.
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import logging
 import threading
 import time
-import wave
 from collections import deque
 
 import numpy as np
-import pyaudio
+import scipy
+from scipy.signal import butter, sosfiltfilt
 
 logger = logging.getLogger("__name__")
 
@@ -13,6 +13,40 @@ logger = logging.getLogger("__name__")
 def main():
     logging.basicConfig(filename="freq_anal_log.log", level=logging.INFO)
     logger.info("Program started.")
+    file = WavFile()
+    file.read_wav()
+    file.stereo_to_mono()
+
+
+def get_band_data(data, low, high, fs, order=5):
+    # Create a bandpass filter in SOS format
+    sos = butter(order, [low, high], btype="band", fs=fs, output="sos")
+    # Apply zero-phase filtering to prevent time-shifting
+    return sosfiltfilt(sos, data)
+
+
+class WavFile:
+    def __init__(self) -> None:
+        logger.info("Initialising audio holding...")
+        self.data = []
+        self.fs = -1
+
+    def read_wav(self, filename="test.wav"):
+        logger.info("Reading in .wav...")
+        (self.fs, self.data) = scipy.io.wavfile.read(f"./assets/{filename}")
+
+    def stereo_to_mono(self):
+        logger.info("Converting .wav to mono signal...")
+        self.data = self.data.mean(axis=1).astype(self.data.dtype)
+
+    def split_signals(self):
+        bands = {"bass": (20, 250), "mid": (250, 4000), "treble": (4000, 20000)}
+
+        split_signals = {
+            name: get_band_data(self.data, low, high, self.fs)
+            for name, (low, high) in bands.items()
+        }
+        print(split_signals)
 
 
 if __name__ == "__main__":
